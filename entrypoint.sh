e9fbca4959d7526b2a7734045d4707d05c9f2e84
Anas Ansari <anas.ansari@CY-IND-L2245.local>
2026-04-07 22:00:26 +0530

    Admin user password removel used for local test

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,8 +38,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
-User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-print('Superuser created: admin / Admin@123')
 "
 fi
 
